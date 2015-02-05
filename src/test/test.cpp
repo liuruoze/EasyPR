9b00fee1a7f2b6abb8fdd69b36d77ec80e4f7b31
Fenix <liuruoze@163.com>
2015-02-05 11:36:16 +0800

    change test.cpp

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -316,7 +316,7 @@ int test_plate_recognize()
 	cout << "test_plate_recognize" << endl;
 
 	//Mat src = imread("image/plate_locate.jpg");
-	Mat src = imread("image/ÔÁB3RS91.jpg");
+	Mat src = imread("image/test.jpg");
 
 	CPlateRecognize pr;
 	pr.LoadANN("model/ann.xml");
