31bd6fce1080fcde54a073f9177ac2726eac83bb
Micooz <Micooz@hotmail.com>
2015-08-05 10:40:36 +0800

    Added ANN train, svm/ann train now support opencv3.0.

--- src/train/ann_train.cpp
+++ src/train/ann_train.cpp
@@ -1,246 +1,87 @@
-// ann_train.cpp : ann模型的训练文件，主要用在OCR中
+#include "easypr/train/ann_train.h"
 
-#include <opencv2/opencv.hpp>
-#include "easypr/plate_recognize.h"
-#include "easypr/util.h"
+namespace easypr{
 
-#ifdef OS_WINDOWS
-#include <windows.h>
-#endif
+  AnnTrain::AnnTrain(const char* chars_folder, const char* zhchars_folder, const char* xml)
+    :chars_folder_(chars_folder), zhchars_folder_(zhchars_folder), ann_xml_(xml){
 
-#include <vector>
-#include <iostream>
-#include <cstdlib>
+  }
 
-#ifdef OS_WINDOWS
-#include <io.h>
-#endif
+  void AnnTrain::train(const int & neurons /* = 40 */){
+    this->getTrainData();
 
-using namespace easypr;
-using namespace cv;
-using namespace std;
+    cv::Mat layers = {
+      train_data_->getNSamples(),// the input layer
+      neurons, // the neurons
+      sizeof(kChinese) + sizeof(kCharacters) // the output layer
+    };
+    ann_->setLayerSizes(layers);
+    ann_->setTrainMethod(cv::ml::ANN_MLP::TrainingMethods::BACKPROP);
+    ann_->setBackpropWeightScale(0.1);
+    ann_->setBackpropMomentumScale(0.1);
 
-#define HORIZONTAL 1
-#define VERTICAL 0
+    std::cout << "Training ANN model, please wait..." << std::endl;
+    long start = utils::getTimestamp();
+    ann_->train(train_data_);
+    long end = utils::getTimestamp();
+    std::cout << "Training done. Elapse: " << (end - start) / 1000 << std::endl;
 
-CvANN_MLP ann;
+    ann_->save(ann_xml_);
+    std::cout << "Your ANN Model was saved to " << ann_xml_ << std::endl;
+  }
 
-//中国车牌
-const char strCharacters[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                              'A', 'B', 'C', 'D', 'E', 'F', 'G',
-                              'H', /* 没有I */
-                              'J', 'K', 'L', 'M', 'N', /* 没有O */ 'P', 'Q',
-                              'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-const int numCharacter = 34;
-/* 没有I和O,10个数字与24个英文字符之和 */
+  void AnnTrain::getTrainData(){
+    assert(chars_folder_);
+    assert(zhchars_folder_);
+    // create new
+    cv::Mat samples;
+    cv::Mat responses;
 
-//以下都是我训练时用到的中文字符数据，并不全面，有些省份没有训练数据所以没有字符
-//有些后面加数字2的表示在训练时常看到字符的一种变形，也作为训练数据存储
-const std::string strChinese[] = {
-    "zh_cuan" /* 川 */,  "zh_e" /* 鄂 */,    "zh_gan" /* 赣*/,
-    "zh_gan1" /*甘*/,    "zh_gui" /* 贵 */,  "zh_gui1" /* 桂 */,
-    "zh_hei" /* 黑 */,   "zh_hu" /* 沪 */,   "zh_ji" /* 冀 */,
-    "zh_jin" /* 津 */,   "zh_jing" /* 京 */, "zh_jl" /* 吉 */,
-    "zh_liao" /* 辽 */,  "zh_lu" /* 鲁 */,   "zh_meng" /* 蒙 */,
-    "zh_min" /* 闽 */,   "zh_ning" /* 宁 */, "zh_qing" /* 青 */,
-    "zh_qiong" /* 琼 */, "zh_shan" /* 陕 */, "zh_su" /* 苏 */,
-    "zh_sx" /* 晋 */,    "zh_wan" /* 皖 */,  "zh_xiang" /* 湘 */,
-    "zh_xin" /* 新 */,   "zh_yu" /* 豫 */,   "zh_yu1" /* 渝 */,
-    "zh_yue" /* 粤 */,   "zh_yun" /* 云 */,  "zh_zang" /* 藏 */,
-    "zh_zhe" /* 浙 */};
+    std::cout << "Collecting chars in " << chars_folder_ << std::endl;
 
-const int numChinese = 31;
-const int numAll = 65;
+    for (auto i = 0; i < sizeof(kCharacters); ++i){
+      char c = kCharacters[i];
+      char sub_folder[512] = { 0 };
 
-/* 34+20=54 */
+      sprintf(sub_folder, "%s/%c", chars_folder_, c);
+      std::cout << "  >> Featuring characters " << c << " in " << sub_folder << std::endl;
 
-void annTrain(Mat TrainData, Mat classes, int nNeruns) {
-  ann.clear();
-  Mat layers(1, 3, CV_32SC1);
-  layers.at<int>(0) = TrainData.cols;
-  layers.at<int>(1) = nNeruns;
-  layers.at<int>(2) = numAll;
-  ann.create(layers, CvANN_MLP::SIGMOID_SYM, 1, 1);
+      auto chars_files = utils::getFiles(sub_folder);
+      for (auto file : chars_files){
+        auto img = cv::imread(file);
+        auto fps = features(img, kPredictSize);
 
-  // Prepare trainClases
-  // Create a mat with n trained data by m classes
-  Mat trainClasses;
-  trainClasses.create(TrainData.rows, numAll, CV_32FC1);
-  for (int i = 0; i < trainClasses.rows; i++) {
-    for (int k = 0; k < trainClasses.cols; k++) {
-      // If class of data i is same than a k class
-      if (k == classes.at<int>(i))
-        trainClasses.at<float>(i, k) = 1;
-      else
-        trainClasses.at<float>(i, k) = 0;
+        samples.push_back(fps);
+        responses.push_back(i);
+      }
     }
-  }
-  Mat weights(1, TrainData.rows, CV_32FC1, Scalar::all(1));
 
-  // Learn classifier
-  // ann.train( TrainData, trainClasses, weights );
+    std::cout << "Collecting zh-chars in " << zhchars_folder_ << std::endl;
 
-  // Setup the BPNetwork
+    for (auto i = 0; i < sizeof(kChinese); ++i){
+      const char *zhc = kChinese[i];
+      char sub_folder[512] = { 0 };
 
-  // Set up BPNetwork's parameters
-  CvANN_MLP_TrainParams params;
-  params.train_method = CvANN_MLP_TrainParams::BACKPROP;
-  params.bp_dw_scale = 0.1;
-  params.bp_moment_scale = 0.1;
+      sprintf(sub_folder, "%s/%s", zhchars_folder_, zhc);
+      std::cout << "  >> Featuring zh-characters " << zhc << " in " << sub_folder << std::endl;
 
-  // params.train_method=CvANN_MLP_TrainParams::RPROP;
-  // params.rp_dw0 = 0.1;
-  // params.rp_dw_plus = 1.2;
-  // params.rp_dw_minus = 0.5;
-  // params.rp_dw_min = FLT_EPSILON;
-  // params.rp_dw_max = 50.;
+      auto chars_files = utils::getFiles(sub_folder);
+      for (auto file : chars_files){
+        auto img = cv::imread(file);
+        auto fps = features(img, kPredictSize);
 
-  ann.train(TrainData, trainClasses, Mat(), Mat(), params);
-}
-
-int saveTrainData() {
-  cout << "Begin saveTrainData" << endl;
-  Mat classes;
-  Mat trainingDataf5;
-  Mat trainingDataf10;
-  Mat trainingDataf15;
-  Mat trainingDataf20;
-
-  vector<int> trainingLabels;
-  string path = "resources/train/data/chars_recognise_ann/chars2";
-
-  for (int i = 0; i < numCharacter; i++) {
-    cout << "Character: " << strCharacters[i] << "\n";
-    stringstream ss(stringstream::in | stringstream::out);
-    ss << path << "/" << strCharacters[i];
-
-    auto files = Utils::getFiles(ss.str());
-
-    size_t size = files.size();
-	for (size_t j = 0; j < size; j++) {
-      cout << files[j].c_str() << endl;
-      Mat img = imread(files[j].c_str(), 0);
-      Mat f5 = features(img, 5);
-      Mat f10 = features(img, 10);
-      Mat f15 = features(img, 15);
-      Mat f20 = features(img, 20);
-
-      trainingDataf5.push_back(f5);
-      trainingDataf10.push_back(f10);
-      trainingDataf15.push_back(f15);
-      trainingDataf20.push_back(f20);
-      trainingLabels.push_back(i);  //每一幅字符图片所对应的字符类别索引下标
+        samples.push_back(fps);
+        responses.push_back(i + sizeof(kCharacters));
+      }
     }
+    //
+    cv::Mat samples_row;
+    cv::Mat responses_row;
+
+    samples.convertTo(samples_row, CV_32FC1);
+    responses.convertTo(responses_row, CV_32FC1);
+
+    train_data_ = cv::ml::TrainData::create(samples_row, ml::SampleTypes::ROW_SAMPLE, responses_row);
   }
 
-  path = "resources/train/data/chars_recognise_ann/charsChinese";
-
-  for (int i = 0; i < numChinese; i++) {
-    cout << "Character: " << strChinese[i] << "\n";
-    stringstream ss(stringstream::in | stringstream::out);
-    ss << path << "/" << strChinese[i];
-
-    auto files = Utils::getFiles(ss.str());
-
-    int size = files.size();
-    for (int j = 0; j < size; j++) {
-      cout << files[j].c_str() << endl;
-      Mat img = imread(files[j].c_str(), 0);
-      Mat f5 = features(img, 5);
-      Mat f10 = features(img, 10);
-      Mat f15 = features(img, 15);
-      Mat f20 = features(img, 20);
-
-      trainingDataf5.push_back(f5);
-      trainingDataf10.push_back(f10);
-      trainingDataf15.push_back(f15);
-      trainingDataf20.push_back(f20);
-      trainingLabels.push_back(i + numCharacter);
-    }
-  }
-
-  trainingDataf5.convertTo(trainingDataf5, CV_32FC1);
-  trainingDataf10.convertTo(trainingDataf10, CV_32FC1);
-  trainingDataf15.convertTo(trainingDataf15, CV_32FC1);
-  trainingDataf20.convertTo(trainingDataf20, CV_32FC1);
-  Mat(trainingLabels).copyTo(classes);
-
-  FileStorage fs("resources/train/ann_data.xml", FileStorage::WRITE);
-  fs << "TrainingDataF5" << trainingDataf5;
-  fs << "TrainingDataF10" << trainingDataf10;
-  fs << "TrainingDataF15" << trainingDataf15;
-  fs << "TrainingDataF20" << trainingDataf20;
-  fs << "classes" << classes;
-  fs.release();
-
-  cout << "End saveTrainData" << endl;
-
-  return 0;
-}
-
-void saveModel(int _predictsize, int _neurons) {
-  FileStorage fs;
-  fs.open("resources/train/ann_data.xml", FileStorage::READ);
-
-  Mat TrainingData;
-  Mat Classes;
-
-  string training;
-  if (1) {
-    stringstream ss(stringstream::in | stringstream::out);
-    ss << "TrainingDataF" << _predictsize;
-    training = ss.str();
-  }
-
-  fs[training] >> TrainingData;
-  fs["classes"] >> Classes;
-
-  // train the Ann
-  cout << "Begin to saveModelChar predictSize:" << _predictsize
-       << " neurons:" << _neurons << endl;
-
-  long start = Utils::getTimestamp();
-  annTrain(TrainingData, Classes, _neurons);
-
-  long end = Utils::getTimestamp();
-  cout << "Elapse:" << (end - start) / 1000 << endl;
-
-  cout << "End the saveModelChar" << endl;
-
-  string model_name = "resources/train/ann.xml";
-  // if(1)
-  //{
-  //	stringstream ss(stringstream::in | stringstream::out);
-  //	ss << "ann_prd" << _predictsize << "_neu"<< _neurons << ".xml";
-  //	model_name = ss.str();
-  //}
-
-  FileStorage fsTo(model_name, cv::FileStorage::WRITE);
-  ann.write(*fsTo, "ann");
-}
-
-int annMain() {
-  cout << "To be begin." << endl;
-
-  saveTrainData();
-
-  //可根据需要训练不同的predictSize或者neurons的ANN模型
-  // for (int i = 2; i <= 2; i ++)
-  //{
-  //	int size = i * 5;
-  //	for (int j = 5; j <= 10; j++)
-  //	{
-  //		int neurons = j * 10;
-  //		saveModel(size, neurons);
-  //	}
-  //}
-
-  //这里演示只训练model文件夹下的ann.xml，此模型是一个predictSize=10,neurons=40的ANN模型。
-  //根据机器的不同，训练时间不一样，但一般需要10分钟左右，所以慢慢等一会吧。
-  saveModel(10, 40);
-
-  cout << "To be end." << endl;
-  int end;
-  cin >> end;
-  return 0;
 }
--- src/train/svm_train.cpp
+++ src/train/svm_train.cpp
@@ -1,266 +1,254 @@
-#include "easypr/svm_train.h"
+#include "easypr/train/svm_train.h"
 #include <ctime>
-#include "easypr/core_func.h"
-#include "easypr/util.h"
+#include "easypr/core/core_func.h"
+#include "easypr/util/util.h"
 
 namespace easypr {
 
-Svm::Svm(const char* forward_data_folder, const char* inverse_data_folder)
+  SvmTrain::SvmTrain(const char* forward_data_folder, const char* inverse_data_folder)
     : forward_(forward_data_folder), inverse_(inverse_data_folder) {
-  assert(forward_);
-  assert(inverse_);
-}
-
-void Svm::divide(const char* images_folder, float percentage /* = 0.7 */) {
-  auto files = utils::getFiles(images_folder);
-  if (files.empty()) {
-    std::cout << "No file found in " << images_folder << std::endl;
-    return;
+    assert(forward_);
+    assert(inverse_);
   }
 
-  srand(unsigned(time(NULL)));
-  random_shuffle(files.begin(), files.end());
+  void SvmTrain::train(bool divide /* = true */, float divide_percentage /* = 0.7 */,
+    const char* out_svm_path /* = kDefaultSvmPath */) {
 
-  size_t split_index = size_t(files.size() * percentage);
-  for (size_t i = 0; i < files.size(); ++i) {
-    // TODO: Move files directly to improve efficiency.
-    auto f = files[i];
-    auto file_name = utils::getFileName(f, true);
-    auto image = cv::imread(f);
-    char save_to[255] = {0};
-    assert(!image.empty());
-    if (i < split_index) {
-      sprintf(save_to, "%s/train/%s", images_folder, file_name.c_str());
-    } else {
-      sprintf(save_to, "%s/test/%s", images_folder, file_name.c_str());
+    if (divide) {
+      std::cout << "Dividing data to be trained and tested..." << std::endl;
+      this->divide(forward_, divide_percentage);
+      this->divide(inverse_, divide_percentage);
     }
-    utils::imwrite(save_to, image);
-    std::cout << f << " -> " << save_to << std::endl;
-  }
-}
 
-void Svm::get_train() {
-  std::cout << "Collecting train data..." << std::endl;
+    cv::Ptr<ml::SVM> svm;
 
-  std::vector<int> labels;
-  auto folder = std::string(forward_).append("/train");
+    // 70% training procedure
+    this->getTrain();
 
-  // iterate forward data
-
-  auto files = Utils::getFiles(folder);
-
-  if (files.empty()) {
-    std::cout << "No file found in " << folder << std::endl;
-    return;
-  }
-
-  std::cout << "Collecting train data in " << folder << std::endl;
-  for (auto f : files) {
-    auto image = cv::imread(f);
-
-    auto features = easypr::histeq(image);
-    features = features.reshape(1, 1);
-
-    this->trainingData_.push_back(features);
-    labels.push_back(Label::kForward);  // Note here
-  }
-
-  // iterate inverse data
-
-  files.clear();
-  folder = std::string(inverse_).append("/train");
-  files = Utils::getFiles(folder);
-
-  if (files.empty()) {
-    std::cout << "No file found in " << folder << std::endl;
-    return;
-  }
-
-  std::cout << "Collecting train data in " << folder << std::endl;
-  for (auto f : files) {
-    auto image = cv::imread(f);
-
-    auto features = easypr::histeq(image);
-    features = features.reshape(1, 1);
-
-    this->trainingData_.push_back(features);
-    labels.push_back(Label::kInverse);  // Note here
-  }
-
-  //
-  cv::Mat out;
-  this->trainingData_.convertTo(out, CV_32FC1);
-  out.copyTo(this->trainingData_);
-  cv::Mat(labels).copyTo(this->classes_);
-}
-
-void Svm::get_test() {
-  std::cout << "Tesing preparation..." << std::endl;
-
-  auto folder = std::string(forward_).append("/test");
-  auto files = Utils::getFiles(folder);
-
-  size_t size = files.size();
-  if (0 == size) {
-    std::cout << "No file found in " << folder << std::endl;
-    return;
-  }
-  std::cout << "Prepare testing data in " << folder << std::endl;
-  for (auto f : files) {
-    auto img = cv::imread(f);
-    test_imgaes_.push_back(img);
-    test_labels_.push_back(kForward);
-  }
-
-  //
-
-  folder.clear();
-  folder = std::string(inverse_).append("/test");
-  files = Utils::getFiles(folder);
-
-  size = files.size();
-  if (0 == size) {
-    std::cout << "No file found in " << folder << std::endl;
-    return;
-  }
-  std::cout << "Prepare testing data in " << folder << std::endl;
-  for (auto f : files) {
-    auto img = cv::imread(f);
-    test_imgaes_.push_back(img);
-    test_labels_.push_back(kInverse);
-  }
-}
-
-void Svm::train(bool divide /* = true */, float divide_percentage /* = 0.7 */,
-                bool train /* = true */,
-                const char* out_svm_path /* = NULL */) {
-  if (out_svm_path == NULL) {
-    out_svm_path = "resources/model/svm.xml";
-  }
-
-  if (divide) {
-    std::cout << "Dividing data to be trained and tested..." << std::endl;
-    this->divide(forward_, divide_percentage);
-    this->divide(inverse_, divide_percentage);
-  }
-
-  CvSVM svm;
-
-  // 70% training procedure
-  if (train) {
-    this->get_train();
+    svm = ml::SVM::create();
 
     if (!this->classes_.empty() && !this->trainingData_.empty()) {
       // need to be trained first
-      CvSVMParams SVM_params;
-      SVM_params.svm_type = CvSVM::C_SVC;
-      // SVM_params.kernel_type = CvSVM::LINEAR; //CvSVM::LINEAR;
-      // 线型，也就是无核
-      SVM_params.kernel_type =
-          CvSVM::RBF;  // CvSVM::RBF 径向基函数，也就是高斯核
-      SVM_params.degree = 0.1;
-      SVM_params.gamma = 1;
-      SVM_params.coef0 = 0.1;
-      SVM_params.C = 1;
-      SVM_params.nu = 0.1;
-      SVM_params.p = 0.1;
-      SVM_params.term_crit = cvTermCriteria(CV_TERMCRIT_ITER, 100000, 0.0001);
+      svm->setType(ml::SVM::C_SVC);
+      svm->setKernel(ml::SVM::RBF);
+      svm->setDegree(0.1);
+      svm->setGamma(1);
+      svm->setCoef0(0.1);
+      svm->setC(1);
+      svm->setNu(0.1);
+      svm->setP(0.1);
+      svm->setTermCriteria(cvTermCriteria(CV_TERMCRIT_ITER, 100000, 0.0001));
 
       std::cout << "Generating svm model file, please wait..." << std::endl;
 
       try {
-        // CvSVM svm(trainingData, classes, cv::Mat(), cv::Mat(), SVM_params);
-        svm.train_auto(this->trainingData_, this->classes_, cv::Mat(),
-                       cv::Mat(), SVM_params, 10,
-                       CvSVM::get_default_grid(CvSVM::C),
-                       CvSVM::get_default_grid(CvSVM::GAMMA),
-                       CvSVM::get_default_grid(CvSVM::P),
-                       CvSVM::get_default_grid(CvSVM::NU),
-                       CvSVM::get_default_grid(CvSVM::COEF),
-                       CvSVM::get_default_grid(CvSVM::DEGREE), true);
-      } catch (const cv::Exception& err) {
+        svm->trainAuto(this->trainingData_, 10,
+          ml::SVM::getDefaultGrid(ml::SVM::C),
+          ml::SVM::getDefaultGrid(ml::SVM::GAMMA),
+          ml::SVM::getDefaultGrid(ml::SVM::P),
+          ml::SVM::getDefaultGrid(ml::SVM::NU),
+          ml::SVM::getDefaultGrid(ml::SVM::COEF),
+          ml::SVM::getDefaultGrid(ml::SVM::DEGREE), false);
+      }
+      catch (const cv::Exception& err) {
         std::cout << err.what() << std::endl;
       }
 
-      utils::mkdir(out_svm_path);
-      cv::FileStorage fsTo(out_svm_path, cv::FileStorage::WRITE);
-      svm.write(*fsTo, "svm");
+      svm->save(out_svm_path);
 
       std::cout << "Generate done! The model file is located at "
-                << out_svm_path << std::endl;
-    } else {
-      // don't train, use ready-made model file
-      try {
-        svm.load("resources/train/svm.xml", "svm");
-      } catch (const cv::Exception& err) {
-        std::cout << err.what() << std::endl;
-      }
+        << out_svm_path << std::endl;
     }
-  }  // if train
-
-  // TODO Check whether the model file exists or not.
-  svm.load(out_svm_path, "svm");  // make sure svm model was loaded
-
-  // 30% testing procedure
-  this->get_test();
-
-  std::cout << "Testing..." << std::endl;
-
-  double count_all = test_imgaes_.size();
-  double ptrue_rtrue = 0;
-  double ptrue_rfalse = 0;
-  double pfalse_rtrue = 0;
-  double pfalse_rfalse = 0;
-
-  size_t label_index = 0;
-  for (auto image : test_imgaes_) {
-    //调用回调函数决定特征
-    auto features = easypr::histeq(image);
-    features = features.reshape(1, 1);
-    cv::Mat out;
-    features.convertTo(out, CV_32FC1);
-
-    Label predict = ((int)svm.predict(out)) == 1 ? kForward : kInverse;
-    Label real = test_labels_[label_index++];
-
-    if (predict == kForward && real == kForward) ptrue_rtrue++;
-    if (predict == kForward && real == kInverse) ptrue_rfalse++;
-    if (predict == kInverse && real == kForward) pfalse_rtrue++;
-    if (predict == kInverse && real == kInverse) pfalse_rfalse++;
   }
 
-  std::cout << "count_all: " << count_all << std::endl;
-  std::cout << "ptrue_rtrue: " << ptrue_rtrue << std::endl;
-  std::cout << "ptrue_rfalse: " << ptrue_rfalse << std::endl;
-  std::cout << "pfalse_rtrue: " << pfalse_rtrue << std::endl;
-  std::cout << "pfalse_rfalse: " << pfalse_rfalse << std::endl;
+  void SvmTrain::runTest(const char* svm_path /* = kDefaultSvmPath */){
+    // TODO Check whether the model file exists or not.
+    auto svm = Algorithm::load<ml::SVM>(svm_path);
 
-  double precise = 0;
-  if (ptrue_rtrue + ptrue_rfalse != 0) {
-    precise = ptrue_rtrue / (ptrue_rtrue + ptrue_rfalse);
-    std::cout << "precise: " << precise << std::endl;
-  } else {
-    std::cout << "precise: "
-              << "NA" << std::endl;
+    this->getTest();
+
+    std::cout << "Testing..." << std::endl;
+
+    double count_all = test_imgaes_.size();
+    double ptrue_rtrue = 0;
+    double ptrue_rfalse = 0;
+    double pfalse_rtrue = 0;
+    double pfalse_rfalse = 0;
+
+    size_t label_index = 0;
+    for (auto image : test_imgaes_) {
+      //调用回调函数决定特征
+      auto features = easypr::histeq(image);
+      features = features.reshape(1, 1);
+      cv::Mat out;
+      features.convertTo(out, CV_32FC1);
+
+      Label predict = ((int)svm->predict(out)) == 1 ? kForward : kInverse;
+      Label real = test_labels_[label_index++];
+
+      if (predict == kForward && real == kForward) ptrue_rtrue++;
+      if (predict == kForward && real == kInverse) ptrue_rfalse++;
+      if (predict == kInverse && real == kForward) pfalse_rtrue++;
+      if (predict == kInverse && real == kInverse) pfalse_rfalse++;
+    }
+
+    std::cout << "count_all: " << count_all << std::endl;
+    std::cout << "ptrue_rtrue: " << ptrue_rtrue << std::endl;
+    std::cout << "ptrue_rfalse: " << ptrue_rfalse << std::endl;
+    std::cout << "pfalse_rtrue: " << pfalse_rtrue << std::endl;
+    std::cout << "pfalse_rfalse: " << pfalse_rfalse << std::endl;
+
+    double precise = 0;
+    if (ptrue_rtrue + ptrue_rfalse != 0) {
+      precise = ptrue_rtrue / (ptrue_rtrue + ptrue_rfalse);
+      std::cout << "precise: " << precise << std::endl;
+    }
+    else {
+      std::cout << "precise: "
+        << "NA" << std::endl;
+    }
+
+    double recall = 0;
+    if (ptrue_rtrue + pfalse_rtrue != 0) {
+      recall = ptrue_rtrue / (ptrue_rtrue + pfalse_rtrue);
+      std::cout << "recall: " << recall << std::endl;
+    }
+    else {
+      std::cout << "recall: "
+        << "NA" << std::endl;
+    }
+
+    double Fsocre = 0;
+    if (precise + recall != 0) {
+      Fsocre = 2 * (precise * recall) / (precise + recall);
+      std::cout << "Fsocre: " << Fsocre << std::endl;
+    }
+    else {
+      std::cout << "Fsocre: "
+        << "NA" << std::endl;
+    }
   }
 
-  double recall = 0;
-  if (ptrue_rtrue + pfalse_rtrue != 0) {
-    recall = ptrue_rtrue / (ptrue_rtrue + pfalse_rtrue);
-    std::cout << "recall: " << recall << std::endl;
-  } else {
-    std::cout << "recall: "
-              << "NA" << std::endl;
+  void SvmTrain::divide(const char* images_folder, float percentage /* = 0.7 */) {
+    auto files = utils::getFiles(images_folder);
+    if (files.empty()) {
+      std::cout << "No file found in " << images_folder << std::endl;
+      return;
+    }
+
+    srand(unsigned(time(NULL)));
+    random_shuffle(files.begin(), files.end());
+
+    size_t split_index = size_t(files.size() * percentage);
+    for (size_t i = 0; i < files.size(); ++i) {
+      // TODO: Move files directly to improve efficiency.
+      auto f = files[i];
+      auto file_name = utils::getFileName(f, true);
+      auto image = cv::imread(f);
+      char save_to[255] = { 0 };
+      assert(!image.empty());
+      if (i < split_index) {
+        sprintf(save_to, "%s/train/%s", images_folder, file_name.c_str());
+      }
+      else {
+        sprintf(save_to, "%s/test/%s", images_folder, file_name.c_str());
+      }
+      utils::imwrite(save_to, image);
+      std::cout << f << " -> " << save_to << std::endl;
+    }
   }
 
-  double Fsocre = 0;
-  if (precise + recall != 0) {
-    Fsocre = 2 * (precise * recall) / (precise + recall);
-    std::cout << "Fsocre: " << Fsocre << std::endl;
-  } else {
-    std::cout << "Fsocre: "
-              << "NA" << std::endl;
+  void SvmTrain::getTrain() {
+    std::cout << "Collecting train data..." << std::endl;
+
+    cv::Mat samples;
+    cv::Mat responses;
+    auto folder = std::string(forward_).append("/train");
+
+    // iterate forward data
+
+    auto files = Utils::getFiles(folder);
+
+    if (files.empty()) {
+      std::cout << "No file found in " << folder << std::endl;
+      return;
+    }
+
+    std::cout << "Collecting train data in " << folder << std::endl;
+    for (auto f : files) {
+      auto image = cv::imread(f);
+
+      auto features = easypr::histeq(image);
+      features = features.reshape(1, 1);
+
+      samples.push_back(features);
+      responses.push_back(Label::kForward);  // Note here
+    }
+
+    // iterate inverse data
+
+    files.clear();
+    folder = std::string(inverse_).append("/train");
+    files = Utils::getFiles(folder);
+
+    if (files.empty()) {
+      std::cout << "No file found in " << folder << std::endl;
+      return;
+    }
+
+    std::cout << "Collecting train data in " << folder << std::endl;
+    for (auto f : files) {
+      auto image = cv::imread(f);
+
+      auto features = easypr::histeq(image);
+      features = features.reshape(1, 1);
+
+      samples.push_back(features);
+      responses.push_back(Label::kInverse);  // Note here
+    }
+
+    cv::Mat samples_row;
+    cv::Mat responses_row;
+
+    samples.convertTo(samples_row, CV_32FC1);
+    responses.convertTo(responses_row, CV_32FC1);
+
+    this->trainingData_ = cv::ml::TrainData::create(samples_row, ml::SampleTypes::ROW_SAMPLE, responses_row);
   }
-}
+
+  void SvmTrain::getTest() {
+    std::cout << "Tesing preparation..." << std::endl;
+
+    auto folder = std::string(forward_).append("/test");
+    auto files = Utils::getFiles(folder);
+
+    size_t size = files.size();
+    if (0 == size) {
+      std::cout << "No file found in " << folder << std::endl;
+      return;
+    }
+    std::cout << "Prepare testing data in " << folder << std::endl;
+    for (auto f : files) {
+      auto img = cv::imread(f);
+      test_imgaes_.push_back(img);
+      test_labels_.push_back(kForward);
+    }
+
+    folder.clear();
+    folder = std::string(inverse_).append("/test");
+    files = Utils::getFiles(folder);
+
+    size = files.size();
+    if (0 == size) {
+      std::cout << "No file found in " << folder << std::endl;
+      return;
+    }
+    std::cout << "Prepare testing data in " << folder << std::endl;
+    for (auto f : files) {
+      auto img = cv::imread(f);
+      test_imgaes_.push_back(img);
+      test_labels_.push_back(kInverse);
+    }
+  }
 
 }  // namespace easypr
