dd00e60d55c3d0ecccaf5dfaa96e8c83056ddb6c
Micooz <micooz@hotmail.com>
2016-06-21 11:42:44 +0800

    fix(CMake): SOURCE_FILES

--- build.sh
+++ build.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 
-build_tmp="_build/"
+__dirname=`dirname $0`
+BUILD_TMP_PATH=${__dirname}/_build
 
-mkdir ${build_tmp} 2>/dev/null
-cd ${build_tmp}
+mkdir -p ${BUILD_TMP_PATH} && cd ${BUILD_TMP_PATH}
 
-cmake --clean-first ..
-make -j 4 --makefile Makefile
+# Generate Makefile
+cmake --clean-first -DCMAKE_BUILD_TYPE=Release ..
+
+# Compile
+if [ -f 'Makefile' ];then
+  make --jobs 4
+fi
+
+# Copy binary files to bin/
+BINARY_PATH=../bin
+
+cp libeasypr.a ${BINARY_PATH}
+cp demo_linux_amd64 ${BINARY_PATH}
+
+# Run demo program
+cd .. && bin/demo_linux_amd64
