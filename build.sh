3554cd7c0d06585a376069625e647adcd72191f4
liuruoze <liuruoze@163.com>
2017-06-19 21:16:31 +0800

    * change readme, build.sh, linux minimum required OpenCV 3.2

--- build.sh
+++ build.sh
@@ -1,31 +1,7 @@
 #!/usr/bin/env bash
 
-cd `dirname $0`
-__dirname=`pwd`
-PLATFORM=`uname | awk '{print tolower($1)}'`
-BUILD_TMP_PATH=${__dirname}/_build
-
-BINARY_NAME=demo_${PLATFORM}_amd64
-BINARY_DIR=${__dirname}/bin
-BINARY_FULL_PATH=${BINARY_DIR}/${BINARY_NAME}
-
-BUILD_BINARY_FULL_PATH=${BUILD_TMP_PATH}/${BINARY_NAME}
-
-# cleanup
-if [ -f ${BUILD_BINARY_FULL_PATH} ];then
-  rm ${BUILD_BINARY_FULL_PATH}
-fi
-
-# create tmp dir for cmake
-mkdir -p ${BUILD_TMP_PATH}
-
-# Generate Makefile
-cd ${BUILD_TMP_PATH}
-  cmake --clean-first -DCMAKE_BUILD_TYPE=Release ${__dirname}
-cd ${__dirname}
-
-# Compile
-if [ $? -eq 0 ];then
-  make --jobs 4 --directory ${BUILD_TMP_PATH}
-fi
-
+rm -rf build
+mkdir build
+cd build
+cmake ..
+make -j8
